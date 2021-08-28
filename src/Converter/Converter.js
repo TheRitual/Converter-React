0f2747897d9c299867f0ca7cd69e2aba3f736717
Ritual <dev@theritual.eu>
2021-08-28 22:43:04 +0200

    changed "for" to "htmlFor"

--- src/Converter/Converter.js
+++ src/Converter/Converter.js
@@ -5,17 +5,17 @@ const Converter = () => (
         <h2 className="converter__header">Converter</h2>
         <form className="converter__form">
             <p>
-                <label for="currencyField">Currency: </label><select id="currencyList"
+                <label htmlFor="currencyField">Currency: </label><select id="currencyList"
                     className="converter__select"></select>
             </p>
             <p>
                 <input type="number" value="1.00" min="0" pattern="^\d{1,6}(\.\d{1,2})?$" id="valuePLN" className="converter__field" autofocus />
-                <label for="valuePLN">PLN</label>
+                <label htmlFor="valuePLN">PLN</label>
             </p>
 
             <p>
                 <input type="number" value="1.00" min="0" pattern="^\d{1,6}((\.|\,)\d{1,2})?$" id="currencyField" className="converter__field" />
-                <label for="currencyField">CODE</label>
+                <label htmlFor="currencyField">CODE</label>
             </p>
             <p className="converter__paragraph">
                 CODE rate: <span className="converter__rate">Loading Ratio</span>
