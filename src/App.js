094412c4c925e8f5929d99cfe77f93d3bec34a26
Ritual <dev@theritual.eu>
2021-08-31 22:17:44 +0200

    moved functions to external modules

--- src/App.js
+++ src/App.js
@@ -1,80 +1,34 @@
-import { useState, useEffect, useCallback } from "react";
 import Converter from "./Converter";
 import Header from "./Header";
 import List from "./List";
 import "./App.css";
-
-const getInitialList = () => {
-  const localStorageList = localStorage.getItem("savedList");
-  return (
-    localStorageList ?
-    JSON.parse(localStorageList) :
-    []
-  );
-}
-
+import { useFetchData } from "./utils/useFetchData";
+import { useConverterStates } from "./utils/useConverterStates"
 
 function App() {
-  const [savedList, setSavedList] = useState(getInitialList());
-  const [currencyList, setCurrencyList] = useState([{ currency: "LOADING", code: "USD", bid: 6.66, ask: 6.66 }]);
-  const [dataReceived, setDataReceived] = useState(false);
-  const [rate, setRate] = useState(3.8551);
-  const [currency, setCurrency] = useState("USD");
-  const [valuePLN, setValuePLN] = useState(1.00);
-  const [valueCUR, setValueCUR] = useState(1.00);
-
-  const getBid = (currency = "USD") => currencyList.filter(cur => cur.code === currency)[0].bid;
-
-  useEffect(() => {
-    localStorage.setItem("savedList", JSON.stringify(savedList));
-  }, [savedList]);
-
-  const fetchData = useCallback(async () => {
-    if (!dataReceived) {
-      console.log("Connecting to NBP");
-      try {
-        const response = await fetch("https://api.nbp.pl/api/exchangerates/tables/C/?format=json");
-        const jsonData = await response.json();
-        console.log("Response received");
-        setDataReceived(true);
-        setCurrencyList(jsonData[0].rates);
-        setCurrency(jsonData[0].rates[0].code);
-        setRate(jsonData[0].rates[0].bid);
-        setValuePLN(Number(rate).toFixed(2));
-      } catch (error) {
-        console.log("Can't connect to NBP");
-        console.log(error);
-      }
-    } else {
-      return null;
-    }
-    // eslint-disable-next-line
-  }, []);
-
-  useEffect(() => {
-    fetchData();
-    // eslint-disable-next-line
-  }, []);
+  const states = useConverterStates();
+  useFetchData(states);
+  const getBid = (currency = "USD") => states.currencyList.filter(cur => cur.code === currency)[0].bid;
 
   return (
     <main>
       <Header />
       <div className="box">
         <Converter
-          setCurrency={setCurrency}
-          currency={currency}
-          valuePLN={valuePLN}
-          setValuePLN={setValuePLN}
-          valueCUR={valueCUR}
-          setValueCUR={setValueCUR}
+          setCurrency={states.setCurrency}
+          currency={states.currency}
+          valuePLN={states.valuePLN}
+          setValuePLN={states.setValuePLN}
+          valueCUR={states.valueCUR}
+          setValueCUR={states.setValueCUR}
           getBid={getBid}
-          currencyList={currencyList}
-          setRate={setRate}
-          rate={rate}
-          savedList={savedList}
-          setSavedList={setSavedList}
+          currencyList={states.currencyList}
+          setRate={states.setRate}
+          rate={states.rate}
+          savedList={states.savedList}
+          setSavedList={states.setSavedList}
         />
-        <List savedList={savedList} setSavedList={setSavedList} />
+        <List savedList={states.savedList} setSavedList={states.setSavedList} />
       </div>
     </main>
   );
