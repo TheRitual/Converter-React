3b0d2720a41f291b5db100ad61dad78ef81ae9b0
Ritual <dev@theritual.eu>
2021-09-01 19:08:50 +0200

    added styled components to Header component

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,8 +1,8 @@
-import "./style.css";
+import { StyledHeader } from "./styled";
 
 const Header = () => (
-    <header className="header">
-        <h1 className="header__header">Currency Exchange</h1>
+    <header>
+        <StyledHeader>Currency Exchange</StyledHeader>
     </header>
 );
 
